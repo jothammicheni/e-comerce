2503c59d387974447843bb14f512b0c3f3b62f3d
jothammicheni <joythammurimi2001@gmail.com>
2023-10-20 00:54:26 +0300

    second commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,14 +3,27 @@ import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 const Home = () => {
   const [data, setData] = useState([]);
+   const[filterItem,setFilteItem]=useState('')
 
+//filter items//
+   function filterItems(input) {
+    const regex = new RegExp(`^${input}`, 'i');
+    return data.filter(item => regex.test(item.itemName));
+  }
+
+  //console.log(filteredItems.length)
+
+  //print items//
   useEffect(() => {
     try {
       axios.get('http://localhost/online_shop_database/sell_products.php')
         .then(response => {
           const result = response.data;
           console.log(result);
-          setData(result);
+        
+            setData(result);
+          
+          
         })
         .catch(error => {
           console.error('Error fetching product data:', error);
@@ -20,6 +33,28 @@ const Home = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (filterItem) {
+      const filteredItems = filterItems(filterItem);
+      setData(filteredItems);
+    } else {
+      // If filterItem is empty, reset data to original unfiltered state
+      try {
+        axios.get('http://localhost/online_shop_database/sell_products.php')
+          .then(response => {
+            const result = response.data;
+            setData(result);
+          })
+          .catch(error => {
+            console.error('Error fetching product data:', error);
+          });
+      } catch (error) {
+        console.error('Error fetching product data:', error);
+      }
+    }
+  }, [filterItem]);
+
+  
   return (
     <div className='row' style={{ background: 'whit' }}>
        <div className='row bg-successs ' style={{background:''}}>
@@ -27,16 +62,19 @@ const Home = () => {
           <input className='mt-3' type='text' 
            style={{height:'30px',width:'100%',outline:'none',borderRadius:'5px',textAlign:'center',border:'none'}}
             placeholder='Search for an Item here'
+            value={filterItem} 
+            onChange={(e)=>setFilteItem(e.target.value)}
            />
         </div>
         <div className='col' style={{maxWidth:'200px',width:'200px'}}>
-         <Button className='mt-2 ' style={{width:'100%'}}>search</Button>
+         <Button className='mt-2 ' style={{width:'100%'}}  onClick={()=>filterItems(filterItem)}>search</Button>
         </div>
       </div>
 
     <div className=' row mt-2' style={{ background: 'white' }}>   
      
-      {data.map(item => (
+      { data.length>=1?(
+        data.map(item => (
         <div key={item.itemid}  className='col m-3' style={{maxWidth:'300px', width:'300px'}}>
         <div
          className='row'
@@ -52,7 +90,11 @@ const Home = () => {
 
           </div>
         </div>
-      ))}
+      ))
+      ):(
+        <h1>Items not available at the moment</h1>
+      )
+      }
         
        
       </div>
