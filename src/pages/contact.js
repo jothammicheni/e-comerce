083510160d6370ef24f1a7ca18311b54e0e044de
jothammicheni <jothammurimi2001@gmail.com>
2023-10-22 01:10:08 +0300

    commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='mt-4'  style={{background: 'white'}}>
-        contact us on on this
+        contact us on on this one
     </div>
   )
 }
